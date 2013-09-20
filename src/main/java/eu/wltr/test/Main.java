6373304bb4d903c47b688738a8e28ffc1ecdb61c
Sven Walter <sven@wltr.eu>
2013-09-20 15:11:54 +0200

    Fixed custom ObjectMapper.

--- src/main/java/eu/wltr/test/Main.java
+++ src/main/java/eu/wltr/test/Main.java
@@ -14,23 +14,35 @@ import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ContainerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
+
 public class Main {
 	public static void main(String[] args) {
 		try {
+			// create custom ObjectMapper
+			ObjectMapper mapper = new ObjectMapper();
+			mapper.enable(SerializationFeature.INDENT_OUTPUT);
+
+			// create JsonProvider to provide custom ObjectMapper
+			JacksonJaxbJsonProvider provider = new JacksonJaxbJsonProvider();
+			provider.setMapper(mapper);
+
+			// configure REST service
 			ResourceConfig rc = new ResourceConfig();
 			rc.register(ExampleResource.class);
-			rc.register(ObjectMapperResolver.class);
+			rc.register(provider);
 
+			// create Grizzly instance and add handler
 			HttpHandler handler = ContainerFactory.createContainer(
 					GrizzlyHttpContainer.class, rc);
-
 			URI uri = new URI("http://0.0.0.0:8080/");
-
 			HttpServer server = GrizzlyHttpServerFactory.createHttpServer(uri);
-
 			ServerConfiguration config = server.getServerConfiguration();
 			config.addHttpHandler(handler, "/");
 
+			// start
 			server.start();
 			System.in.read();
 
